b85d9e07b452ccd859f2f46fba704cf5e68ad18b
tanel.ismael <tanel.ismael@khk.ee>
2018-12-13 13:06:46 +0200

    väljastamine

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # PHPalused
-test
---
+++ print/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tanel.ismael
+ * Date: 13.12.2018
+ * Time: 12:46
+ */
+
+//teksti väljastamine
+print('hello my brudda <br>');
+print('php alused <br>');
+//teksti väljastamine echoga
+echo 'my ninja <br>';
+echo 'uus katse <br>';
+// väärtuste ühendamine. järnev lause on pandud kokku kahest lausest
+echo 'proovin asju kokku panna vms ' . 'noh ja mis siit tuleb siis';
+
+
+?>
