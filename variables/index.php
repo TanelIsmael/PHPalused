cf02b36b6f7af71ab49d8ad10fc79c59788c70f4
tanel.ismael <tanel.ismael@khk.ee>
2018-12-13 13:15:09 +0200

    muutujad

---
+++ variables/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tanel.ismael
+ * Date: 13.12.2018
+ * Time: 13:07
+ */
+
+// $ muutuja nimi = väärtus
+$taisarv = 3;
+$reaalarv = 3.3;
+$string = "kolm";
+$toevaartus = true;
+$toevaartus = false;
+
+echo 'taisarv ' . $taisarv . ' väärtus';
+
+
+?>
