b02387c154d3aae359a0b7d3a47a717d87e5a872
tanel.ismael <tanel.ismael@khk.ee>
2018-12-18 15:15:45 +0200

    massiivid

--- arrays/index.php
+++ arrays/index.php
@@ -6,5 +6,65 @@
  * Time: 14:42
  */
 
+/*$kasutajad = array(
+    'Pets',
+    'Guits',
+    'Ants'
+);*/
+
+$kasutajad = array();
+
+$kasutajad[] = 'alice';
+$kasutajad[] = 'bob';
+
+array_push($kasutajad, 'lucy');
+
+echo '<pre>';
+var_dump($kasutajad);
+echo '</pre>';
+
+echo '<pre>';
+print_r($kasutajad);
+echo '</pre>';
+
+//for ($i = 0; $i < count($kasutajad); $i++){
+// echo $i. ' - '.$kasutajad[$i].'<br>'; }
+
+foreach ($kasutajad as $kasutaja) {
+    echo $kasutaja . '<br>';
+}
+
+echo '<br>';
+array_shift($kasutajad); //lükkab esimese välja
+array_pop($kasutajad); //lükkab viimase välja
+
+foreach ($kasutajad as $kasutaja) {
+    echo $kasutaja . '<br>';
+}
+
+
+?>
+
+<?php
+
+$kasutajad = array(
+    array(
+        'Pets',
+        'Guits',
+        'Ants'
+    ),
+    array(
+        'Pets',
+        'Guits',
+        'Ants'
+    )
+
+);
+
+echo '<br>';
+echo '<pre>';
+print_r($kasutajad);
+echo '</pre>';
+
 
 ?>
