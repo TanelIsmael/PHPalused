d6ea3a4a8f1ad14a185994ae44563de079faa571
tanel.ismael <tanel.ismael@khk.ee>
2019-01-24 14:41:59 +0200

    vormi töötlus faili asukoht

---
+++ session/acts/login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tanel.ismael
+ * Date: 24.01.2019
+ * Time: 14:34
+ */
+
+// andmed html vormi poolt
+$username = $_GET ['uname'];
+$password = $_GET ['upass'];
+
+echo $username . ' ' . $password;
