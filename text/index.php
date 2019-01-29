fd937416a4fd0b7456de9822ead7929f09f02d67
tanel.ismael <tanel.ismael@khk.ee>
2019-01-29 11:29:34 +0200

    text kui massiiv

--- text/index.php
+++ text/index.php
@@ -37,3 +37,36 @@ echo strip_tags($tekst);    //Experience is the teacher of fools
 echo '<br>';
 $tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
 echo strip_tags($tekst, '<b>, <br>');    //<b>Experience</b> is the teacher <br>of fools
+
+echo '<br>';
+
+$tekst = 'All thinking men are atheists';
+echo $tekst[0];                //A
+echo '<br>';
+echo $tekst[4];                //t
+
+echo '<br>';
+
+$tekst = 'All thinking men are atheists';
+echo substr($tekst, 3, 5);        //thin
+echo '<br>';
+echo substr($tekst, 4, -13);    //thinking men
+echo '<br>';
+echo substr($tekst, -8, 7);        //atheist
+
+echo '<br>';
+
+$tekst = 'All thinking men are atheists';
+print_r(str_word_count($tekst, 1));        //Array ( [0] => All [1] => thinking [2] => men [3] => are [4] => atheists )
+
+echo '<br>';
+
+$tekst = 'All thinking men are atheists';
+$sona = str_word_count($tekst, 1);
+echo $sona[2];                            //men
+
+echo '<br>';
+
+$tekst = 'All thinking men are atheists';
+print_r(str_word_count($tekst, 2));
+//Array ( [0] => All [4] => thinking [13] => men [17] => are [21] => atheists )
