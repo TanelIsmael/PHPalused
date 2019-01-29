9e059968817a08da8296266c31ec73bbe27a540a
tanel.ismael <tanel.ismael@khk.ee>
2019-01-29 11:24:38 +0200

    texti pikkus

--- text/index.php
+++ text/index.php
@@ -15,3 +15,10 @@ echo strtoupper($tekst);
 echo '<br>';
 $tekst = 'Life Is About Ignoring The Drama.';
 echo ucfirst(strtolower($tekst));
+
+echo '<br>';
+
+$tekst = 'Experience is the teacher of fools';
+echo strlen($tekst);            //34
+echo '<br>';
+echo str_word_count($tekst);    //6
