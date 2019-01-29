4205954e529b4325d3c56eee273718c8bb8074fb
tanel.ismael <tanel.ismael@khk.ee>
2019-01-29 11:23:44 +0200

    texti vormindamine

--- text/index.php
+++ text/index.php
@@ -5,3 +5,13 @@
  * Date: 29.01.2019
  * Time: 11:20
  */
+
+$tekst = 'Life Is About Ignoring The Drama.';
+
+//muudab texti kas upper või lower case
+echo strtolower($tekst);
+echo '<br>';
+echo strtoupper($tekst);
+echo '<br>';
+$tekst = 'Life Is About Ignoring The Drama.';
+echo ucfirst(strtolower($tekst));
