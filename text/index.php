deecb234aa1a08cb3476532ae14b88af25d71e96
tanel.ismael <tanel.ismael@khk.ee>
2019-01-29 11:27:08 +0200

    texti kärpimine

--- text/index.php
+++ text/index.php
@@ -22,3 +22,18 @@ $tekst = 'Experience is the teacher of fools';
 echo strlen($tekst);            //34
 echo '<br>';
 echo str_word_count($tekst);    //6
+
+
+$tekst = ' 	A woman should soften but not weaken a man   ';
+echo "<pre>$tekst</pre>";
+echo "<pre>" . trim($tekst) . "</pre>";
+echo "<pre>" . ltrim($tekst) . "</pre>";
+echo "<pre>" . rtrim($tekst) . "</pre>";
+$tekst = 'A woman should soften but not weaken a man';
+echo trim($tekst, "A, a, k..n, w");    //oman should soften but not weake
+echo '<br>';
+$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
+echo strip_tags($tekst);    //Experience is the teacher of fools
+echo '<br>';
+$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
+echo strip_tags($tekst, '<b>, <br>');    //<b>Experience</b> is the teacher <br>of fools
